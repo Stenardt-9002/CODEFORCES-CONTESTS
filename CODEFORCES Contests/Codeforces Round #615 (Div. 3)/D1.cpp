f8151ad3272b3592ef0b42d053d82fe8385a9633
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2020-01-22 22:12:35 +0530

    Incomplete c try again during discussion -a

---
+++ CODEFORCES Contests/Codeforces Round #615 (Div. 3)/D1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+#define MAX_VAL 1000000000
+typedef long long int lld;
+
+
+int main(int argc, char const *argv[])
+{
+
+  lld queries,xval;
+  lld *input,*output,*record;
+  input = new lld[queries];
+  record = new lld[queries];
+  output = new lld[queries];
+
+  for (lld i = 0; i < queries; i++)
+  {
+
+
+    cin>>input[i];
+
+    if (i>0)
+    {
+      //begin move
+      /* code */
+    }
+
+
+
+  }
+
+  return 0;
+}
