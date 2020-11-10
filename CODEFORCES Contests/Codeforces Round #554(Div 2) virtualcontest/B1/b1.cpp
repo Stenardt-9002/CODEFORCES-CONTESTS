5722675da48ac54ec51d84e53adec39aa7ae4c4a
Stenardt-9002 <esd17i014@iiitdm.ac.in>
2020-11-11 02:11:28 +0530

    Round #554 part2

--- CODEFORCES Contests/Codeforces Round #554(Div 2) virtualcontest/B1/b1.cpp
+++ CODEFORCES Contests/Codeforces Round #554(Div 2) virtualcontest/B1/b1.cpp
@@ -1,14 +1,38 @@
+//
 
+#include<cmath>
 #include<iostream>
 
 
+typedef long long int lld;
+
 using namespace std;
 
-
+int pwer2fuc(lld val1)
+{
+    // float val1 = log2(val);
+    if (val1 ==0 )
+    {
+        return false ;
+    }
+    return (ceil(log2(val1))==floor(log2(val1)));
+    
+}
 
 int main(int argc, char const *argv[])
 {
     
+    lld x ;
+    cin>>x ; 
+
+    if (pwer2fuc(x))
+    {
+        
+    }
+    
+
+
+
     return 0;
 }
 
